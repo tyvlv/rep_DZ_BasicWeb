a3e126f2a753de7e566b3243538e61c84031b4a7
tychkov.vv <tyvl@yandex.ru>
2023-06-03 02:13:48 +0300

    Do homework 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
 ]
